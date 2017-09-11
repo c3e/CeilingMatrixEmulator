1b219f97268fc65d68c19ad92c994ef5f2eb4d25
elektrospy <elektrospy@gmail.com>
2017-09-11 22:26:58 +0200

    its rendering :D

--- MatrixEmulator.py
+++ MatrixEmulator.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 import pty
-import serial
+# import serial
 from random import randint
 
 pixelWidth = 8
@@ -14,6 +14,9 @@ panelPixelY = 8
 
 gridPanelX = 10
 gridPanelY = 5
+
+# calculate complete number of pixels
+pixelBufferNumber = (panelPixelX * panelPixelY) * (gridPanelX * gridPanelY) * 3
 
 
 class pixel:
@@ -43,31 +46,58 @@ class grid:
         self.Matrix = [[panel(self.panelPixelX, self.panelPixelY, self.panelOrder) for x in range(self.panelX)] for y in range(self.panelY)]
 
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-serialMaster, serialSlave = pty.openpty()
-s_name = os.ttyname(serialSlave)
-serialDevice = serial.Serial(s_name)
-
-
-def writeSerialTestText():
-    global serialDevice
+class SerialEmulator:
+    # master, slave = pty.openpty()
+    # s_name = os.ttyname(slave)
+    # ser = serial.Serial(s_name)
     # To Write to the device
-    serialDevice.write('Your text')
-
-
-def readSerialBuffer():
-    global serialMaster
+    # ser.write('Your text')
     # To read from the device
-    returnBuffer = os.read(serialMaster, 1)
-    if returnBuffer == '':
-        return 0
-    else:
-        return int(returnBuffer)
+    # os.read(master, 1000)
+
+    def __init__(self):
+        self.master, self.slave = pty.openpty()
+        self.s_name = os.ttyname(self.slave)
+        print("Hey use this serial port:", self.s_name)
+
+    def writeSerialTestText(self):
+        # To Write to the device
+        self.master.write('Your text')
+
+    def readSerialBuffer(self):
+        # To read from the device
+        returnBuffer = os.read(self.master, 1)
+
+        if returnBuffer == '':
+            return 0
+        else:
+            # print ord(returnBuffer)
+            return ord(returnBuffer)
+
+
+virtualSerial = SerialEmulator()
+pixelBuffer = [0] * pixelBufferNumber
+
+
+def updatePixelBuffer():
+    global virtualSerial
+    global pixelBufferNumber
+    global pixelBuffer
+
+    # int idx = 0;
+    # for (size_t i = 0; i < NUM_LINES*NUM_LEDS_PER_LINE; i++) {
+    #   pixelbuffer[idx++] = gammaCorrection[serialGlediator()]; // G
+    #   pixelbuffer[idx++] = gammaCorrection[serialGlediator()]; // R
+    #   pixelbuffer[idx++] = gammaCorrection[serialGlediator()]; // B
+    #   pixelbuffer[idx++] = gammaCorrection[whiteStart];        // W
+    # }
+
+    while virtualSerial.readSerialBuffer() != 1:
+        # lets wait some until sync
+        pass
+
+    for currentPixelInBuffer in range(pixelBufferNumber):
+        pixelBuffer[currentPixelInBuffer] = virtualSerial.readSerialBuffer()
 
 
 def main():
@@ -100,7 +130,48 @@ def main():
 
         currentPixel = 0  # just to keep track where we are
 
-        # print str(readSerialBuffer())
+        updatePixelBuffer()
+        # print "Buffer update done."
+
+        global pixelBuffer
+
+        global panelPixelX
+        global panelPixelY
+
+        valuesPerPanel = panelPixelX * panelPixelY * 3
+
+        currentPanelX = 0
+        currentPanelY = 0
+
+        for bufferIndex in range(len(pixelBuffer)):
+            if bufferIndex % valuesPerPanel == 0:
+                if bufferIndex == 0:
+                    pass
+                else:
+                    currentPanelX = currentPanelX + 1
+                    currentX = 0
+                    currentY = 0
+                # else:
+                # pass
+
+            if currentPanelX == panelGrid.panelX:
+                currentPanelX = 0
+                currentPanelY = currentPanelY + 1
+
+            if bufferIndex % 3 == 0:
+                red = pixelBuffer[bufferIndex]
+                green = pixelBuffer[bufferIndex + 1]
+                blue = pixelBuffer[bufferIndex + 2]
+                # print("Matrix[", currentPanelY, "][", currentPanelX, "].Matrix[", currentX, "][", currentY, "] = [", red, ", ", green, ", ", blue, "]", bufferIndex)
+                panelGrid.Matrix[currentPanelY][currentPanelX].Matrix[currentX][currentY].color = [red, green, blue]
+                currentX = currentX + 1
+
+            if currentX == panelPixelX:
+                currentY = currentY + 1
+                currentX = 0
+
+            if currentY == panelPixelY:
+                currentY = 0
 
         # print(x[i][j])
         # Draw the grid
@@ -120,13 +191,14 @@ def main():
                         currentX = (pixelMargin + pixelWidth) * currentPixelX + pixelMargin + currentPanelOffsetX
                         currentY = (pixelMargin + pixelHeight) * currentPixelY + pixelMargin + currentPanelOffsetY
 
-                        RED = randint(0, 255)
-                        GREEN = randint(0, 255)
-                        BLUE = randint(0, 255)
+                        # RED = randint(0, 255)
+                        # GREEN = randint(0, 255)
+                        # BLUE = randint(0, 255)
 
                         # draw that dirty little pixel
-                        pygame.draw.rect(screen, [RED, GREEN, BLUE], [currentX, currentY, pixelWidth, pixelHeight])
-                        pygame.draw.rect(screen, [255, 255, 255], [currentX, currentY, pixelWidth, pixelHeight], 1)
+                        # pygame.draw.rect(screen, [RED, GREEN, BLUE], [currentX, currentY, pixelWidth, pixelHeight])
+                        pygame.draw.rect(screen, panelGrid.Matrix[currentPanelY][currentPanelX].Matrix[currentPixelX][currentPixelY].color, [currentX, currentY, pixelWidth, pixelHeight])
+                        # pygame.draw.rect(screen, [255, 255, 255], [currentX, currentY, pixelWidth, pixelHeight], 1)
 
                         # print 'Pa: ' + str(currentPanelX) + ' ' + str(currentPanelY)
                         # print 'Pi: ' + str(currentPixelX) + ' ' + str(currentPixelY)
